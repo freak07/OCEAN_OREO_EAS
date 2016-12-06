d2a605839006f5fae74b409ece37354bc44e0abb
Kevin Brodsky <kevin.brodsky@arm.com>
2016-12-06 08:03:43 -0800

    UPSTREAM: arm64: compat: Remove leftover variable declaration
    (cherry-pick from commit 82d24d114f249d919b918ff8eefde4117db8f088)
    
    Commit a1d5ebaf8ccd ("arm64: big-endian: don't treat code as data when
    copying sigret code") moved the 32-bit sigreturn trampoline code from
    the aarch32_sigret_code array to kuser32.S. The commit removed the
    array definition from signal32.c, but not its declaration in
    signal32.h. Remove the leftover declaration.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Bug: 19198045
    Bug: 20045882
    Change-Id: Ic8a5f0e367f0ecd5c5ddd9e3885d0285f91cf89e
    (cherry picked from commit a4aa7a668507382c6eabc73ba6cd73059a0c8f51)
    (cherry picked from commit a93c8c17e6edd7234f0bd963d645f25422d8ed61)

--- arch/arm64/include/asm/signal32.h
+++ arch/arm64/include/asm/signal32.h
@@ -66,8 +66,6 @@ struct compat_rt_sigframe {
 	struct compat_sigframe		sig;
 };
 
-extern const compat_ulong_t aarch32_sigret_code[6];
-
 int compat_setup_frame(int usig, struct ksignal *ksig, sigset_t *set,
 		       struct pt_regs *regs);
 int compat_setup_rt_frame(int usig, struct ksignal *ksig, sigset_t *set,
