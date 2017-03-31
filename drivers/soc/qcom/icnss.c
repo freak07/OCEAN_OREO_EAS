24a71030bce44bfcbf93893e1c04038d002a72e3
Hardik Kantilal Patel <hkpatel@codeaurora.org>
2017-03-31 13:02:47 +0530

    icnss: pre-alloc memory leak check on probe failure and remove
    After wlan host driver probe failure and driver unload check the
    memory leak and reset the memory pool.
    
    CRs-Fixed: 2027407
    Change-Id: Id6e413416ec7ea1d1bf0e69aab7eccb06de753d7
    Signed-off-by: Hardik Kantilal Patel <hkpatel@codeaurora.org>
    (cherry picked from commit 0e7de47c30178a647042a7abc7b4f30cc602c274)

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -49,6 +49,11 @@
 #include <soc/qcom/socinfo.h>
 #include <soc/qcom/ramdump.h>
 
+#ifdef CONFIG_WCNSS_MEM_PRE_ALLOC
+#include <net/cnss_prealloc.h>
+#endif
+
+
 #include "wlan_firmware_service_v01.h"
 
 #ifdef CONFIG_ICNSS_DEBUG
@@ -2120,6 +2125,8 @@ static int icnss_call_driver_probe(struct icnss_priv *priv)
 	if (ret < 0) {
 		icnss_pr_err("Driver probe failed: %d, state: 0x%lx\n",
 			     ret, priv->state);
+		wcnss_prealloc_check_memory_leak();
+		wcnss_pre_alloc_reset();
 		goto out;
 	}
 
@@ -2251,6 +2258,8 @@ static int icnss_driver_event_register_driver(void *data)
 	if (ret) {
 		icnss_pr_err("Driver probe failed: %d, state: 0x%lx\n",
 			     ret, penv->state);
+		wcnss_prealloc_check_memory_leak();
+		wcnss_pre_alloc_reset();
 		goto power_off;
 	}
 
@@ -2275,6 +2284,8 @@ static int icnss_driver_event_unregister_driver(void *data)
 		penv->ops->remove(&penv->pdev->dev);
 
 	clear_bit(ICNSS_DRIVER_PROBED, &penv->state);
+	wcnss_prealloc_check_memory_leak();
+	wcnss_pre_alloc_reset();
 
 	penv->ops = NULL;
 
@@ -2299,6 +2310,8 @@ static int icnss_call_driver_remove(struct icnss_priv *priv)
 	penv->ops->remove(&priv->pdev->dev);
 
 	clear_bit(ICNSS_DRIVER_PROBED, &priv->state);
+	wcnss_prealloc_check_memory_leak();
+	wcnss_pre_alloc_reset();
 
 	icnss_hw_power_off(penv);
 
