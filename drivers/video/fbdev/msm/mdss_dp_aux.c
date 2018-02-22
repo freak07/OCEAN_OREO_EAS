b1d55b8d02bb6ab9d180c6e5d27374b93d16c55f
freak07 <michalechner92@googlemail.com>
2018-02-22 22:19:03 +0100

    Revert "tree: sec update Dec 2017 / u11 2.42.xxx.2 base"
    This reverts commit f718391682389c48c20671b4b34dbcb3bac46997.
    
    (cherry picked from commit 007b2cec524e1279fac0c720ab1437d0e3ff40e3)
    (cherry picked from commit a82e5a95402f22a856a9479a266de737f52ac723)

--- drivers/video/fbdev/msm/mdss_dp_aux.c
+++ drivers/video/fbdev/msm/mdss_dp_aux.c
@@ -461,12 +461,6 @@ retry:
 		goto end;
 	}
 
-	if (!dp->dp_initialized) {
-		pr_err("DP not initialized!\n");
-		ret = -ENODEV;
-		goto end;
-	}
-
 	/* Adjust AUX configuration and retry */
 	pr_debug("AUX failure (%d), adjust AUX settings\n", ret);
 	mdss_dp_phy_aux_update_config(dp, PHY_AUX_CFG1);
